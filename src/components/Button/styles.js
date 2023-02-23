2a9140392074af3d5d7e97106883d93c6e3444c9
Dione Santos <dionedev@gmail.com>
2023-02-22 22:56:35 -0300

    feat: created button component

---
+++ src/components/Button/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  width: 100%;
+  height: 4.8rem;
+  background-color: ${({ theme }) => theme.colors.tomato_100};
+  border: none;
+  border-radius: 8px;
+  color: ${({ theme }) => theme.colors.light_100};
+  font-size: 1.4rem;
+`
