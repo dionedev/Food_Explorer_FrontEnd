b78f5f5ec7da9f59da57f45080a67167bcc563a1
Dione Santos <dionedev@gmail.com>
2023-03-01 10:29:17 -0300

    updating repository for sharing in forum explorer

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,42 @@
-import { Logo } from "../Logo"
-import { Container } from "./styles"
-import MenuIcon from "../../assets/Icons/Menu.svg"
 import ReceiptIcon from "../../assets/Icons/Receipt.svg"
+import SignOutIcon from "../../assets/Icons/SignOut.svg"
+import MenuIcon from "../../assets/Icons/Menu.svg"
+import { Container, Search, MobileContent } from "./styles"
+import { Input } from "../Input"
+import { Logo } from "../Logo"
+import { Button } from "../Button"
 
 export function Header() {
   return(
     <Container>
-      <img src={MenuIcon} alt="ícone do menu" />
-      <Logo/>
-      <img src={ReceiptIcon} alt="ícone de recibo" />
+      <MobileContent className="mobile-content">
+        <button>
+          <img src={MenuIcon} alt="ícone do menu" />
+        </button>
+
+        <Logo/>
+
+        <button>
+          <img src={ReceiptIcon} alt="ícone de recibo" />
+        </button>
+      </MobileContent>
+      
+      {/* <DesktopContent>
+        <Logo />
+
+        <Search>
+          <Input 
+            type="text" 
+            placeholder="Busque por pratos ou ingredientes" 
+          />
+        </Search>
+
+        <Button title="Pedidos" />
+
+        <button>
+        <img src={SignOutIcon} alt="ícone para sair da aplicação" />
+        </button>
+      </DesktopContent> */}
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,21 +1,39 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
+  grid-area: header;
   width: 100%;
-  height: 11.4rem;
+  height: 11rem;
   background-color: ${({ theme }) => theme.colors.dark_700};
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
-  align-items: center;
   padding: 6.4rem 0 3.2rem;
-  
 
-  > img {
-    width: 2.6rem;
-    height: 2.2rem;
+  @media (min-width: 768px) {
+    .mobile-content {
+      background-color: red;
+    }
+  }
+`
+
+export const MobileContent = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  // Ícones menu e recibo
+  > button {
+    background: transparent;
+    border: none;
+
+    img { 
+      width: 2.6rem;
+      height: 2.2rem;
+    }
   }
 
+  // Logo
   > div {
     width: 27.8rem;
     height: 2.6rem;
@@ -29,5 +47,13 @@ export const Container = styled.header`
       font-size: 2.4rem;
     }
   }
+`
 
+export const Search = styled.div`
+  width: 100%;
+  background-color: aqua;
+
+  > div {
+    
+  }
 `
